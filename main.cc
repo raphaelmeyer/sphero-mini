fa0ba34c35c23bae500dc2eb78861852d9a60876
raphael meyer <raphael.akira.meyer@gmail.com>
2019-03-29 10:11:37 +0100

    switch to plain dbus

--- main.cc
+++ main.cc
@@ -1,23 +1,55 @@
-#include <dbus/dbus-glib.h>
+#include <dbus/dbus.h>
 
-#include <cassert>
+#include <iostream>
+#include <stdexcept>
 
 int main() {
-  GError *error = nullptr;
-  auto conn = dbus_g_bus_get(DBUS_BUS_SESSION, &error);
-  assert(not error);
-  assert(conn);
 
-  auto proxy = dbus_g_proxy_new_for_name_owner(
-    conn,
-    "org.bluez",
-    "/org/bluez/hci0/dev_B0_B4_48_BD_D0_83",
-    "org.bluez.Device1",
-    &error
-  );
-  assert(proxy);
-  assert(not error);
+  DBusError dbus_error;
+  const char * dbus_result = nullptr;
+
+  ::dbus_error_init(&dbus_error);
+
+  auto dbus_conn = ::dbus_bus_get(DBUS_BUS_SYSTEM, &dbus_error);
+  if(not dbus_conn) {
+    throw std::runtime_error{dbus_error.message};
+  }
+
+  auto dbus_msg =
+    ::dbus_message_new_method_call("org.freedesktop.DBus", "/", "org.freedesktop.DBus.Introspectable", "Introspect");
+  if(not dbus_msg) {
+    ::dbus_connection_unref(dbus_conn);
+    throw std::runtime_error{dbus_error.message};
+  }
+
+  auto dbus_reply =
+    ::dbus_connection_send_with_reply_and_block(dbus_conn, dbus_msg, DBUS_TIMEOUT_USE_DEFAULT, &dbus_error);
+  if(not dbus_reply) {
+    ::dbus_message_unref(dbus_msg);
+    ::dbus_connection_unref(dbus_conn);
+    throw std::runtime_error{dbus_error.message};
+  }
+
+  if(not::dbus_message_get_args(dbus_reply, &dbus_error, DBUS_TYPE_STRING, &dbus_result, DBUS_TYPE_INVALID)) {
+    ::dbus_message_unref(dbus_reply);
+    ::dbus_message_unref(dbus_msg);
+    ::dbus_connection_unref(dbus_conn);
+    throw std::runtime_error{dbus_error.message};
+  }
+
+  std::cout << "Connected to D-Bus as \"" << ::dbus_bus_get_unique_name(dbus_conn) << "\"." << std::endl;
+  std::cout << "Introspection Result:" << std::endl << std::endl;
+  std::cout << dbus_result << std::endl;
+  ::dbus_message_unref(dbus_msg);
+  ::dbus_message_unref(dbus_reply);
+
+  ::dbus_connection_unref(dbus_conn);
+
+  /*
+      "org.bluez",
+      "/org/bluez/hci0/dev_FF_57_53_15_5E_4E",
+      "org.bluez.Device1",
+  */
 
   return 0;
 }
-
